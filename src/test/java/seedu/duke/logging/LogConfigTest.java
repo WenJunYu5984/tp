fe8b42a02b618c1d7be5b89e6a11061457a9a07b
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-15 23:55:12 +0800

    Checkstyle violations

--- src/test/java/seedu/duke/logging/LogConfigTest.java
+++ src/test/java/seedu/duke/logging/LogConfigTest.java
@@ -1,6 +1,8 @@
 package seedu.duke.logging;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import java.util.logging.Logger;
